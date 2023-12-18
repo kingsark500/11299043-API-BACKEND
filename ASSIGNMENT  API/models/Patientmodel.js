0fa245bfc0fc97cfc258a6cc62f7bd2a61e00ee1
maxwell sarkodie <kingsark500@gmail.com>
2023-12-18 08:29:52 +0000

    added patient models

---
+++ ASSIGNMENT  API/models/Patientmodel.js
@@ -0,0 +1,30 @@
+const { default: mongoose } = require("mongoose");
+
+const {Schema} = mongoose
+
+const patientSchema = new Schema({
+
+    PatientID: String,
+
+    Surname: String,
+
+    Othernames: String,
+
+    Gender: { type : String , enum: ['Male', 'Female'] },
+    
+    PhoneNumber: Number,
+
+    ResidentialAddress: String,
+
+    EmergencyName: String,
+
+    Contact: Number,
+
+    RelationshipWithStudent: String
+
+})
+
+
+const Patient = mongoose.model('Patient', patientSchema);
+
+module.exports= Patient;
--- ASSIGNMENT  API/server.js
+++ ASSIGNMENT  API/server.js
@@ -1,25 +1,15 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const app = express();
 
-//routes
+app.use(bodyParser.json());
 
-app.get('/', (req, res)=> {
-    res.send('Hello blog my name is maxwell')
-})
+mongoose.connect('mongodb://localhost/hospital', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch((err) => console.error('Could not connect to MongoDB...', err));
 
-
-
-mongoose.connect('mongodb+srv://admin:xdedse@cluster0.5acm9qs.mongodb.net/Node-API?retryWrites=true&w=majority')
-.then(()=> {
-    app.listen(3000, ()=>{
-    console.log("Node api app is running on port 3000")
-})
-    //connecting to the database
-    console.log('connected to Mongodb')
-}).catch(()=> {
-    console.log('error could not connect to mongodb')
-})
+app.listen(3000, () => console.log('Hospital server  running on port 3000...'));
 
 
